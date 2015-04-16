1e8449d9ace34251fba0536d308de3dff4e617b3
Avishaan <avishaan@codeHatcher.com>
2015-04-15 18:55:47 -0700

    add default placeholder and filter image view cell

--- ExchangeAGram/FilterCell.swift
+++ ExchangeAGram/FilterCell.swift
@@ -9,5 +9,20 @@
 import UIKit
 
 class FilterCell: UICollectionViewCell {
+  
+  var imageView:UIImageView!
+  
+  // custom initializer so we can have the cell immediately setup the image view when it is created
+  override init(frame: CGRect) {
+    // we want the UICollectionViewCell to do it's own init, after we add our own custom functionality
+    super.init(frame: frame)
     
+    imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
+    contentView.addSubview(imageView)
+  }
+  
+  // make this class NSCoding compliant
+  required init(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+  }
 }
--- ExchangeAGram/FilterViewController.swift
+++ ExchangeAGram/FilterViewController.swift
@@ -23,15 +23,17 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
     
     let layout = UICollectionViewFlowLayout()
     // add some insets
-    layout.sectionInset = UIEdgeInsetsMake(10, 10, 10, 10)
+    layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
     layout.itemSize = CGSize(width: 150.0, height: 150.0)
     collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
     
     // set delegate datasource, normally we drag in storyboard
     collectionView.dataSource = self
     collectionView.delegate = self
-    
     collectionView.backgroundColor = UIColor.whiteColor()
+    // register cell with the class
+    collectionView.registerClass(FilterCell.self, forCellWithReuseIdentifier: "MyCell")
+    
     self.view.addSubview(collectionView)
     
   }
@@ -42,11 +44,15 @@ class FilterViewController: UIViewController, UICollectionViewDataSource, UIColl
   }
   
   func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    return 0
+    return 2
   }
   
   func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-    return UICollectionViewCell()
+    
+    let cell:FilterCell = collectionView.dequeueReusableCellWithReuseIdentifier("MyCell", forIndexPath: indexPath) as FilterCell
+    cell.imageView.image = UIImage(named: "Placeholder")
+    
+    return cell
   }
   
 }
