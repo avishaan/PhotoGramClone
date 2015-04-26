3c58f49f8844676f4463bd3e7f36731be2d174f5
Avishaan <avishaan@codeHatcher.com>
2015-04-25 18:21:04 -0700

    setup bridging for facebook login as well as url request in the delegate

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Brown Magic on 4/24/15.
+//  Copyright (c) 2015 codeHatcher. All rights reserved.
+//
+
+#import <FacebookSDK/FacebookSDK.h>
