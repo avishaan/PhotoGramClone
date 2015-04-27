b4d5cf7dd4f12887ccc56e708088fbebc5c45104
Avishaan <avishaan@codeHatcher.com>
2015-04-27 16:46:23 -0700

    get feeditems that are stored via the request

--- ExchangeAGram/MapViewController.swift
+++ ExchangeAGram/MapViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import MapKit
+import CoreData
 
 class MapViewController: UIViewController {
   
@@ -15,21 +16,33 @@ class MapViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    
     // Do any additional setup after loading the view.
-    let location = CLLocationCoordinate2D(latitude: 48.868639224587, longitude: 2.37119161036255)
-    // determine how much map to show
-    let span = MKCoordinateSpanMake(0.05, 0.05)
-    // region to show
-    let region = MKCoordinateRegionMake(location, span)
-    mapView.setRegion(region, animated: true)
+    // query for the feeditems
+    let request = NSFetchRequest(entityName: "FeedItem")
     
-    let annotation = MKPointAnnotation()
-    annotation.setCoordinate(location)
-    annotation.title = "Canal Saint-Martin"
-    annotation.subtitle = "Paris"
+    //acces app delegate
+    let appDelegate:AppDelegate = (UIApplication.sharedApplication().delegate as AppDelegate)
+    //get context
+    let context = appDelegate.managedObjectContext!
+    var error:NSError?
+    let itemArray = context.executeFetchRequest(request, error: &error)
+    println(error)
     
-    // put annotation on map
-    mapView.addAnnotation(annotation)
+//    let location = CLLocationCoordinate2D(latitude: 48.868639224587, longitude: 2.37119161036255)
+//    // determine how much map to show
+//    let span = MKCoordinateSpanMake(0.05, 0.05)
+//    // region to show
+//    let region = MKCoordinateRegionMake(location, span)
+//    mapView.setRegion(region, animated: true)
+//    
+//    let annotation = MKPointAnnotation()
+//    annotation.setCoordinate(location)
+//    annotation.title = "Canal Saint-Martin"
+//    annotation.subtitle = "Paris"
+//    
+//    // put annotation on map
+//    mapView.addAnnotation(annotation)
     
   }
   
