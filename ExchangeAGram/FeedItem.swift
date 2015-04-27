236b1840952faa58620994cd9f6e7540028f178d
Avishaan <avishaan@codeHatcher.com>
2015-04-27 15:39:15 -0700

    update coredata model with the gps coordinate information

--- ExchangeAGram/FeedItem.swift
+++ ExchangeAGram/FeedItem.swift
@@ -2,18 +2,20 @@
 //  FeedItem.swift
 //  ExchangeAGram
 //
-//  Created by Brown Magic on 4/15/15.
+//  Created by Brown Magic on 4/27/15.
 //  Copyright (c) 2015 codeHatcher. All rights reserved.
 //
 
 import Foundation
 import CoreData
-
 @objc (FeedItem)
+
 class FeedItem: NSManagedObject {
 
     @NSManaged var caption: String
     @NSManaged var image: NSData
     @NSManaged var thumbnail: NSData
+    @NSManaged var latitude: NSNumber
+    @NSManaged var longitude: NSNumber
 
 }
